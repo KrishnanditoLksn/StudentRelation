6e8c13c51e8dfcd68b831e67d6245a64d783ddc3
Gorthdzx <krislaksana123@gmail.com>
2024-11-05 10:39:09 +0700

    feat : added isGraduate column

--- app/src/main/java/app/ditodev/studentrelation/db/StudentDb.kt
+++ app/src/main/java/app/ditodev/studentrelation/db/StudentDb.kt
@@ -3,8 +3,10 @@ package app.ditodev.studentrelation.db
 import android.content.Context
 import androidx.room.AutoMigration
 import androidx.room.Database
+import androidx.room.RenameColumn
 import androidx.room.Room
 import androidx.room.RoomDatabase
+import androidx.room.migration.AutoMigrationSpec
 import app.ditodev.studentrelation.dao.StudentDao
 import app.ditodev.studentrelation.entity.Course
 import app.ditodev.studentrelation.entity.Student
@@ -12,10 +14,17 @@ import app.ditodev.studentrelation.entity.University
 
 @Database(
     entities = [Student::class, University::class, Course::class],
-    version = 1,
+    version = 3,
+    autoMigrations = [
+        AutoMigration(from = 1, to = 2),
+        AutoMigration(from = 2, to = 3, spec = StudentDatabase.AutoMigration::class)
+    ],
     exportSchema = true,
 )
 abstract class StudentDatabase : RoomDatabase() {
+
+    @RenameColumn(tableName = "Student", fromColumnName = "graduate", toColumnName = "isGraduate")
+    class AutoMigration : AutoMigrationSpec
 
     abstract fun studentDao(): StudentDao
 
